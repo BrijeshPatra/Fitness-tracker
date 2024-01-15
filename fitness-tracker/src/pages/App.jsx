825801a8f2a59d569a17ee3f214f803995826328
Brijesh patra <brijeshpatra25@gmail.com>
2024-01-15 11:38:23 +0530

    Initial commit

---
+++ fitness-tracker/src/pages/App.jsx
@@ -0,0 +1,22 @@
+// App.jsx
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
